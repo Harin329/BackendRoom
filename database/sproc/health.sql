8aec359d86ce02381145e72ea0e93943d2013a4e
Hao (Harin) Wu <46661377+Harin329@users.noreply.github.com>
2021-09-15 11:05:48 -0700

    Update health.sql

--- database/sproc/health.sql
+++ database/sproc/health.sql
@@ -5,7 +5,7 @@ DROP procedure IF EXISTS `postHealth`;
 
 DELIMITER $$
 USE `health_db`$$
-CREATE PROCEDURE `postHealth`(IN `input_name` VARCHAR(255), `my_id` INT)
+CREATE PROCEDURE `postHealth`(IN `input_name` VARCHAR(255), IN `my_id` INT)
 BEGIN
 
 INSERT INTO `health_table` (`log_id`, `name`, `time`) VALUES (`my_id` `input_name`, now());
