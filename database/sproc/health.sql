5797644ac56db3fccb8c744acee08e836d9ba6c0
Hao (Harin) Wu <46661377+Harin329@users.noreply.github.com>
2021-09-15 11:09:52 -0700

    Update health.sql

--- database/sproc/health.sql
+++ database/sproc/health.sql
@@ -8,7 +8,7 @@ USE `health_db`$$
 CREATE PROCEDURE `postHealth`(IN `input_name` VARCHAR(255), IN `my_id` INT)
 BEGIN
 
-INSERT INTO `health_table` (`log_id`, `name`, `time`) VALUES (`my_id` `input_name`, now());
+INSERT INTO `health_table` (`log_id`, `name`, `time`) VALUES (`my_id`, `input_name`, now());
 
 END$$
 
